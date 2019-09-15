e492d9878e92602aa2e2d73840c4291d42cda38b
Jason Koo <jkoo2088@gmail.com>
2019-09-14 23:59:52 -0400

    added completed problem

---
+++ dmoj/3/AnotherContest4Problem1_IdentityCrisis.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include <string>
+
+using namespace std;
+
+string f(int N) {
+    std::string s = std::to_string(N);
+    return s;
+}
+
+int main() {
+    f(10);
+    return 0;
+}
