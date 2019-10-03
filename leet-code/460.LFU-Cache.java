e60c1125d1521de756963e23a3f0efdaf4ea6b3d
Jason Koo <jkoo2088@gmail.com>
2019-10-03 18:54:16 -0400

    finished 460 LFU Cache ques

--- leet-code/460.LFU-Cache.java
+++ leet-code/460.LFU-Cache.java
@@ -1,12 +1,70 @@
+import java.util.*;
 class LFUCache {
-    public LFUCache(){
+    HashMap<Integer, Integer> values;
+    HashMap<Integer, Integer> counts;
+    HashMap<Integer, LinkedHashSet<Integer>> lists;
+    int capacity = 0;
+    int min = -1;
 
+    public LFUCache(int capacity) {
+        this.capacity = capacity;
+        values = new HashMap<>();
+        counts = new HashMap<>();
+        lists = new HashMap<>();
+        lists.put(1, new LinkedHashSet<>());
     }
-    public int get(int key){
 
+    public int get(int key) {
+        if(!values.containsKey(key)) {
+            return -1;
+        }
+        int count = counts.get(key);
+        counts.put(key, count + 1);
+        lists.get(count).remove(key);
+        if (count == min && lists.get(count).size() == 0) {
+            min++;
+        }
+        if(!lists.containsKey(count + 1)) {
+           lists.put(count + 1, new LinkedHashSet<>());
+        }
+        lists.get(count + 1).add(key);
+        return values.get(key);
     }
-    public void put(int key, int value){
+    
+    public void put(int key, int value) {
+         if(capacity <= 0) {
+            return;
+        }
+        if(values.containsKey(key)) {
+            values.put(key, value);
+            get(key);
+            return;
+        }
+        if(values.size() >= capacity) {
+            int evit = lists.get(min).iterator().next();
+            lists.get(min).remove(evit);
+            values.remove(evit);
+            counts.remove(evit);
+        }
+        values.put(key, value);
+        counts.put(key, 1);
+        min = 1;
+        lists.get(1).add(key);
+    }
+    public static void main(String[] args) {
+        LFUCache cache = new LFUCache(2);
+        cache.put(1, 1);
+        cache.put(2, 2);
+        cache.get(1);       
+        cache.put(3, 3);    
+        cache.get(2);       
+        cache.get(3);    
+        cache.put(4, 4);    
+        cache.get(1);       
+        cache.get(3);    
+        cache.get(4);       
 
+        
     }
 }
 /*
