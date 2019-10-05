dd86ecc17032b9c470276d7bcbe78134f92762fd
Jason Koo <jkoo2088@gmail.com>
2019-10-05 10:25:06 -0400

    added #212.py

---
+++ daily-coding-problems/Dropbox/212.py
@@ -0,0 +1,9 @@
+# This problem  was asked by Dropboc
+# Spreadsheets often use this alphabetical encoding for its columns:
+# "A","B","B", ..., "AA", "AB", ..., "ZZ", "AAA", "AAB", ...
+# Given a column number return its alphabetical column id. For example
+# given 1 return "A" given 27, return "AA"
+
+def func() {
+    HashMap<S
+}
